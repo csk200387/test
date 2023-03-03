2df34a628753b49e8942ae971436d7a333253b3a
csk200387 <csk200387@gmail.com>
2023-03-04 01:51:14 +0900

    [upload]

--- java.java
+++ java.java
@@ -10,3 +10,39 @@ class CodeRunner{
 		}
 	}
 }
+import java.util.Scanner;
+class CodeRunner{
+	public static void main(String[] args){
+		Scanner input = new Scanner(System.in);
+		System.out.print("입력 : ");
+		int num = input.nextInt(); // 정수형 입력
+		String chr = input.next();
+		if(num == 123){
+			System.out.println("int");
+		}
+	}
+}
+import java.util.Scanner;
+class CodeRunner{
+	public static void main(String[] args){
+		Scanner input = new Scanner(System.in);
+		System.out.print("입력 : ");
+		int num = input.nextInt(); // 정수형 입력
+		String chr = input.next();
+		if(num == 123){
+			System.out.println("int");
+		}
+	}
+}
+import java.util.Scanner;
+class CodeRunner{
+	public static void main(String[] args){
+		Scanner input = new Scanner(System.in);
+		System.out.print("입력 : ");
+		int num = input.nextInt(); // 정수형 입력
+		String chr = input.next();
+		if(num == 123){
+			System.out.println("int");
+		}
+	}
+}
